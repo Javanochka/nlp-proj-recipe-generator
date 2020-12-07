39d8420638dddaf3197e5a6dc96f4424398c5afc
Fork my Git <45640841+Prometheos2@users.noreply.github.com>
2020-12-07 10:31:35 +0100

    Identify Ingr: Add a method to break the lists down for identification.

--- ingredient_identification/identify_ingredients.py
+++ ingredient_identification/identify_ingredients.py
@@ -3,6 +3,15 @@ import spacy
 nlp = spacy.load('en_core_web_sm')
 
 
+def split_ingrlst_into_list(tagged_text: str) -> list:
+    """Break the ingredient textual list into a list object."""
+    res = tagged_text.replace("ingrlst itemst ", "")  # Ingredient list's start tag.
+    res = res.replace(" itemfn", "")
+    res = res.split(" itemst ")
+    return res
+    # for instlst: ingrlfn  instrst / sentfn sentst
+
+
 def acquire_ingredients(tagged_text: str) -> list:
     """"""
 
