a38cb748e5965863ce3a131e2c35a8364abe66b3
Fork My Git <45640841+Prometheos2@users.noreply.github.com>
2020-12-24 18:01:13 +0100

    Ingredient diff: Add the instruction sets methods. +Update.

--- ingredient_identification/identify_ingredients.py
+++ ingredient_identification/identify_ingredients.py
@@ -1,29 +1,67 @@
+import re
+
 import spacy
 
-nlp = spacy.load('en_core_web_md')
+# Load the custom model
+nlp = spacy.load('../models')
 
 
 def split_ingrlst_into_list(tagged_text: str) -> list:
-    """Break the ingredient textual list into a list object."""
-    res = tagged_text.replace("ingrlst itemst ", "")  # Ingredient list's start tag.
+    """Break the ingredient set into a list object."""
+    res = tagged_text.replace("ingrlst itemst ", "")  # Ingredient set's start tag.
     res = res.replace(" itemfn", "")
     res = res.split(" itemst ")
     return res
-    # for instlst: ingrlfn  instrst / sentfn sentst
 
 
-def acquire_ingredients(tagged_text: str) -> list:
-    """We collect the ingredient in the text through PRODUCT-type Named Entities."""
+def split_instrst_into_list(tagged_text: str) -> list:
+    """Break the instruction set into a list object."""
+    res = tagged_text.replace("instrst sentst ", "")  # Instruction list's start tag.
+    res = res.replace(" . sentfn", "")
+    res = res.split(" sentst ")
+    return res
+
+
+# List of false positives among the FOOD named entities. TODO: Improve model and delete list.
+FALSE_POSITIVE_LIST = "teaspoon", "chopped"
+
+
+def add_of_to_measure_units(item: str) -> str:
+    """Append the word 'of' to measure units in the items. Allow to have better results with NER."""
+    measure_units = ["teaspoons?", "tablespoons?"]
+    for unit in measure_units:
+        item = re.sub(unit, "\g<0> of", item)
+    return item
+
+
+def acquire_ingredients_from_ingrlst(tagged_text: str) -> list:
+    """We collect the ingredient in the text through FOOD-type Named Entities."""
     curated_ingredients = []
     for item in split_ingrlst_into_list(tagged_text):
-        # For testing — TODO: To erase
-        ingredients = [(ent.text, ent.label_) for ent in nlp(item).ents]
-        # It fails to work — TODO: to fix.
-        # ingredients = [ent.text for ent in nlp(item).ents if ent.label_ == "PRODUCT"]
-        curated_ingredients.append(ingredients)
+        # Remove the measures that create problem for NER
+        item = add_of_to_measure_units(item)
+        # Pick the ingredients found by the NER
+        ingredients = [ent.text for ent in nlp(f"I ate {item}").ents if ent.label_ == "FOOD"
+                       and ent.text not in FALSE_POSITIVE_LIST]
+        if ingredients:
+            curated_ingredients.append(ingredients)
 
     return curated_ingredients
 
 
-def are_ingredients_the_same(tagged_ingredient_list: str, tagged_instructions: str) -> bool:
-    return acquire_ingredients(tagged_ingredient_list) == acquire_ingredients(tagged_instructions)
+def acquire_ingredients_from_instrst(tagged_text: str) -> list:
+    """We collect the ingredient in the text through FOOD-type Named Entities."""
+    curated_ingredients = []
+    for item in split_instrst_into_list(tagged_text):
+        # Pick the ingredients found by the NER
+        ingredients = [ent.text for ent in nlp(item).ents if ent.label_ == "FOOD"
+                       and ent.text not in FALSE_POSITIVE_LIST]
+        if ingredients:
+            curated_ingredients.append(ingredients)
+
+    return curated_ingredients
+
+
+def do_ingredients_correspond(tagged_set: str) -> bool:
+    sets = tagged_set.split("ingrlfn")
+    return acquire_ingredients_from_ingrlst(sets[0]) == acquire_ingredients_from_instrst(sets[1])
--- ingredient_identification/identify_ingredients_test.py
+++ ingredient_identification/identify_ingredients_test.py
@@ -1,13 +1,14 @@
 import unittest
 import identify_ingredients as module
 
-# Sample of an
+# Sample of an ingredient list
 INGREDIENT_LIST_SAMPLE: str = "ingrlst itemst 0.5 (8 ounce) package button mushrooms itemfn itemst 0.5 (6 ounce) package pre-sliced portabella mushrooms itemfn itemst 1 teaspoon olive oil itemfn itemst 1 teaspoon butter itemfn itemst 2 tablespoons finely chopped shallots itemfn itemst 2 garlic cloves, chopped itemfn itemst 18 teaspoon salt itemfn itemst 14 wonton wrappers itemfn itemst 1 teaspoon cornstarch itemfn itemst 12 cup 1% low-fat milk itemfn itemst 1 tablespoon all-purpose flour itemfn itemst 2 tablespoons grated fresh parmesan cheese itemfn itemst 1 tablespoon chopped fresh chives itemfn itemst 18 teaspoon salt itemfn itemst 1 dash fresh ground black pepper itemfn"
+INSTRUCTION_LIST_SAMPLE = "instrst sentst drain the pineapple , then put into a large bowl . sentfn sentst reserve the cup . sentfn sentst meanwhile , combine chocolate chips and sugar in a large bowl . sentfn sentst stir until slightly thickened , about 8 minutes . sentfn sentst add 1 / 2 cup of 1 / 2 cup of the whites and then beat until stiff peaks form , about mixture ) . sentfn sentst brush each half over hot dough by lightly both sides and cut during the baking . sentfn sentst sprinkle with cinnamon and glaze . sentfn sentst cookies sentfn sentst melt chocolate chips and butter over medium heat . sentfn sentst stir together the caramel topping and eggs . sentfn sentst place onto squares and to warmed molds and place on a plate . sentfn sentst run knife around each 8 sides to touch in center of each rectangle . sentfn sentst invert onto well floured surface sprayed with cooking spray , pressing into rounds . sentfn"
 
 
 class IdentifyIngrTest(unittest.TestCase):
-    def test_split_ingredients_ingrlst(self):
-        """Test identify_ingredients' split_elements_into_list method."""
+    def test_split_ingrlst_into_list(self):
+        """Test identify_ingredients' split_ingrlst_into_list method."""
         array = module.split_ingrlst_into_list(INGREDIENT_LIST_SAMPLE)
         print(array)
         self.assertListEqual(array, ['0.5 (8 ounce) package button mushrooms',
@@ -19,12 +20,19 @@ class IdentifyIngrTest(unittest.TestCase):
                                      '2 tablespoons grated fresh parmesan cheese', '1 tablespoon chopped fresh chives',
                                      '18 teaspoon salt', '1 dash fresh ground black pepper'])
 
-    def test_acquire_ingredients_ingrlst(self):
-        """Test the acquire_ingredients method — Currently only printing the result."""
-        array = module.acquire_ingredients(INGREDIENT_LIST_SAMPLE)
+    def test_split_instrst_into_list(self):
+        """Test the split_instrst_into_list method — Currently only printing the result."""
+        array = module.split_instrst_into_list(INSTRUCTION_LIST_SAMPLE)
         print(array)
-        # We currently try to see whether 'PRODUCT' properly includes ingredients.
-        array = module.acquire_ingredients("We will have one teaspoon of butter.")
+
+    def test_acquire_ingredients_from_ingrlst(self):
+        """Test the acquire_ingredients_from_ingrlst method — Currently only printing the result."""
+        array = module.acquire_ingredients_from_ingrlst(INGREDIENT_LIST_SAMPLE)
+        print(array)
+
+    def test_acquire_ingredients_from_instrst(self):
+        """Test the acquire_ingredients_from_instrst method — Currently only printing the result."""
+        array = module.acquire_ingredients_from_instrst(INSTRUCTION_LIST_SAMPLE)
         print(array)
 
     if __name__ == '__main__':
