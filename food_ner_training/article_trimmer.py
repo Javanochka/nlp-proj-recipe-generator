62ffe7428958edc2eb13d16801b6bb2352cd7a78
Fork My Git <45640841+Prometheos2@users.noreply.github.com>
2020-12-24 17:55:48 +0100

    NER Train: Update the article trimmer to add the whole dataset.

--- food_ner_training/article_trimmer.py
+++ food_ner_training/article_trimmer.py
@@ -9,11 +9,11 @@ def remove_artefacts_from_title():
     r[2] = r[2].replace(" - The New York Times", "")
 
 
-with open(f"{SOURCE}.csv", "rt", encoding="utf8") as source:
+with open(f"articles_trimmed.csv", "wt", encoding="utf8", newline='') as result:
+    wtr = csv.writer(result)
     csv.field_size_limit(100000000)
-    rdr = csv.reader(source)
-    with open(f"{SOURCE}_trimmed.csv", "wt", encoding="utf8", newline='') as result:
-        wtr = csv.writer(result)
-        for r in rdr:
-            remove_artefacts_from_title()
-            wtr.writerow((r[0], r[1], r[2]))  # Only keep the ids and the titles.
+    for file in ["articles1", "articles2", "articles3"]:
+        with open(f"{file}.csv", "rt", encoding="utf8") as source:
+            for r in csv.reader(source):
+                remove_artefacts_from_title()
+                wtr.writerow((r[0], r[1], r[2]))  # Only keep the ids and the titles.
